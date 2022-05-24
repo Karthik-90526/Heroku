990f2c766c0ec2c70e72b19d42d4192257274af4
DESKTOP-RISFSMC\gopik <gopikumarreddy.38@gmail.com>
2022-05-24 21:26:19 +0530

    file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express =require('express')
+/*const express =require('express')
 const app=express()
 const port =process.env.PORT || 3000
 app.get('/',(req,res) =>{
@@ -7,4 +7,26 @@ app.get('/',(req,res) =>{
 
  app.listen(3000,()=>{
     console.log('App is listen')
- })
+ })*/
+const express =require('express');
+const app= express();
+const cors = require("cors");
+const helmet = require("helmet");
+const Blog =require('./db/blog')
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+const port =process.env.port || 3000
+app.post('/',(req,res)=>{
+Blog.create(req.body).then((blog)=>{
+res.status(201).send(blog);
+})
+.catch((error)=>{
+    res.status(400).send(error);
+})
+});
+
+app.listen(port,(req,res)=>{
+    console.log("app is running");
+})
